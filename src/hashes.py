b1f53ce0fda75cada0e9b61ca55ffe63e053198e
Landry Irakoze <irakozel@gmail.com>
2020-03-09 13:59:27 -0400

    hashes practice

---
+++ src/hashes.py
@@ -0,0 +1,17 @@
+import hashlib
+
+n = 1
+key = b'my_value'
+key2 = 'string'.encode()
+key3 = b'lunchtime'
+
+# for i in range(n):
+#     print(hash(key))
+#     print(hashlib.sha256(key).hexdigest())
+
+index = hash(key) % 8
+index2 = hash(key2) % 8
+index3 = hash(key3) % 8
+print(index)
+print(index2)
+print(index3)
